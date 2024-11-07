b4ea53fd0e207eb4b8cbbe4fac2d03144a253b88
yousenurf27 <yosenurf@gmail.com>
2024-11-07 07:25:27 +0700

    fix: :bug: bug back home

--- src/app/(pokemon)/[name]/components/Navbar.tsx
+++ src/app/(pokemon)/[name]/components/Navbar.tsx
@@ -1,13 +1,12 @@
-import Link from 'next/link';
 import { BASE_URL } from '../../../../utils/config';
 import { MoveLeft } from 'lucide-react';
 
 const Navbar = () => {
   return (
     <nav className='px-4 py-4 bg-transparent absolute top-0 left-0 right-0'>
-      <Link href={BASE_URL || ''}>
+      <a href={BASE_URL || ''}>
         <MoveLeft className='text-white' />
-      </Link>
+      </a>
     </nav>
   );
 };
